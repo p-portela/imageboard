28a2fbd69eccdf6323194fe95b7a26a51211ea95
Pedro Portela <pportela@disroot.org>
2022-01-07 23:14:02 +0100

    Board page now renderable. Minor fixes.

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from app import app
 from app.forms import PostForm
+from app.models import Board
 
 imageboard_name = 'tchan'       #placeholder
 boardtitle = '/b/ - Random'     #placeholder
@@ -21,7 +22,11 @@ def index():
     ]
     return render_template('index.html', imageboard_name=imageboard_name, news=news)
 
-@app.route('/boardpath', methods=['GET', 'POST'])
+@app.route('/<boardpath>', strict_slashes=False, methods=['GET', 'POST'])
 def board(boardpath):
     form = PostForm()
-    return render_template('board.html', imageboard_name=imageboard_name, boardtitle=boardtitle, subtitle=subtitle)
+    board = Board.query.filter_by(boardpath=boardpath).first_or_404()
+    posts = [
+        {'id': 1, 'body': 'carai doidokkkkkkkkkkkkk'}
+    ]
+    return render_template('board.html', imageboard_name=imageboard_name, boardtitle=boardtitle, subtitle=subtitle, form=form, boardpath=boardpath)
